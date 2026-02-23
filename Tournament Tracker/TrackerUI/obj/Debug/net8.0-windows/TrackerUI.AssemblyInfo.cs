b006a4a7a9482306b06484a290d77346c655468a
Sahil Madaan <madaan.sahil27@gmail.com>
2026-02-23 21:21:10 +0530

    MISC changes

--- Tournament Tracker/TrackerUI/obj/Debug/net8.0-windows/TrackerUI.AssemblyInfo.cs
+++ Tournament Tracker/TrackerUI/obj/Debug/net8.0-windows/TrackerUI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TrackerUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32b3af7725ee46147445743a0b4292db237b5a9c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+636cd43801eaa9c136cd2d10dab31f6c0b67fc3b")]
 [assembly: System.Reflection.AssemblyProductAttribute("TrackerUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TrackerUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
